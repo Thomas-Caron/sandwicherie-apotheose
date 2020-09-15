c9945698d90f025985763197ff722b55c63f9b88
Thomas-Caron <thomas.caron.dev@gmail.com>
2020-09-15 22:16:22 +0200

    missing point in middleware

--- front/src/middlewares/categoriesMiddleware.js
+++ front/src/middlewares/categoriesMiddleware.js
@@ -8,9 +8,8 @@ import { GET_CATEGORIES, saveCategories } from '../actions/categories';
 const categoriesMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_CATEGORIES:
-      axios.get('http://ec2-3-84-167-149compute-1.amazonaws.com/api/api/categories')
+      axios.get('http://ec2-3-84-167-149.compute-1.amazonaws.com/api/api/categories')
         .then((response) => {
-          // console.log(response.data);
           store.dispatch(saveCategories(response.data));
         })
         .catch((error) => {
